8e3440abfc8258ebcb75eed40782b8311e80a8f7
Patrick Forbes <patrickjforbes@students.utech.edu.jm>
2020-09-11 18:55:42 -0500

    Finished oauth flow

---
+++ routes/authRoutes.js
@@ -0,0 +1,29 @@
+const passport = require("passport");
+
+module.exports = (app) => {
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
+  );
+
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", {
+      failureRedirect: "/",
+      successRedirect: "/home",
+    })
+  );
+
+  app.get("/auth/current_user", (req, res) => {
+      console.log(req.us)
+    res.send(req.user);
+  });
+ 
+  app.get("/auth/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+};
